217550f4ff058598c1cecbcdd603fe066f469b93
Justin BABONNEAU <justin.babonneau@ensiie.fr>
2023-01-04 03:08:40 +0100

    Added ';;' after each end of function in analysis.ml

--- analysis.ml
+++ analysis.ml
@@ -2,6 +2,7 @@
 let do_parse cin a =
   let l = input_line cin in
   Scanf.sscanf l a
+;;
 
 
 let analyse_base cin = 
@@ -12,20 +13,23 @@ let analyse_base cin =
          do_parse cin "%s %s %d" (fun a b c -> a,b,c)
        )
     )
-  
+;; 
+
 let analyse_file_1 fn =
   let cin = open_in fn in
   let transitions = analyse_base cin in 
   let start_stop = do_parse cin "%s %s" (fun a b -> a,b) in
   let _ = close_in cin in
   transitions,start_stop
-                
-                
+;;
+          
+
 let output_sol_1 time mod_list =
   Format.printf "%d : %s" time (List.hd mod_list);
   List.iter (Format.printf " -> %s") (List.tl mod_list);
   Format.printf "@."
-    
+;;
+
 
 let split_on_char c s =
   let rec aux i =
@@ -35,19 +39,22 @@ let split_on_char c s =
     with _ -> [String.sub s i (String.length s - i)]
   in
   aux 0
-  
+;;
+
 let parse_trajets cin =
   let m = do_parse cin "%d" (fun x -> x) in
   Array.to_list (Array.init m (fun _ ->
       let s = input_line cin in
       List.filter (fun s -> s<>"" && s<> "->") (split_on_char ' ' s)
     ))
-  
+;;
+    
 let analyse_file_2 fn =
   let cin = open_in fn in
   let transitions = analyse_base cin in
   let trajets = parse_trajets cin in
   transitions,trajets
+;;
 
 let parse_trajets_2 cin =
   let m = do_parse cin "%d" (fun x -> x) in
@@ -57,15 +64,15 @@ let parse_trajets_2 cin =
       | src::dst::[] -> src,dst
       | _ -> raise (Scanf.Scan_failure "format invalide")
     ))
+;;
 
-  
+
 let analyse_file_3 fn =
   let cin = open_in fn in
   let transitions = analyse_base cin in
   let trajets = parse_trajets_2 cin in
   transitions,trajets
-
-  
+;;
 
 let output_sol_2 sol_list =
   List.iter (fun (mvs,sol) ->
@@ -74,3 +81,4 @@ let output_sol_2 sol_list =
       Format.printf "@."
     )
     sol_list
+;;
